9140eca785e3fe444f896b6b7d2748452979a352
Vadim <vad99lord@gmail.com>
2022-08-12 12:52:58 +0300

    Delete unused hooks

--- src/hooks/userNoRenderRef.ts
+++
@@ -1,9 +0,0 @@
-import { useRef } from "react";
-
-const useNoRenderRef = <T>(currentValue: T) => {
-  const ref = useRef(currentValue);
-  ref.current = currentValue;
-  return ref;
-};
-
-export default useNoRenderRef;
